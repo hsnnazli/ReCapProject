247870faa3e15b830f28a1c54af9bd4f74640348
hsnnazli <huseyinnazli06@gmail.com>
2021-02-10 15:20:37 +0300

    son

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,11 +12,22 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
-             
-
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+            Console.WriteLine("--------------Car List-------------- ");
             foreach (var car in carManager.GetCarDetails())
             {
                 Console.WriteLine(car.Id + "-" + car.BrandName + "-" + car.ColarName + "-" +car.DailyPrice);
+            }
+            Console.WriteLine("--------------Brand List-------------- ");
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.BrandId +"-" + brand.BrandName);
+            }
+            Console.WriteLine("--------------Color List-------------- ");
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.ColorId+ "-" + color.ColorName);
             }
             Console.ReadLine();
 
