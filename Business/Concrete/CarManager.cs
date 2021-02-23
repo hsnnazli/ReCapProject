6d28b4bf99cff3c884162e6f6821ce16222972b6
hsnnazli <huseyinnazli06@gmail.com>
2021-02-23 22:59:27 +0300

    FluentValidation

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,10 +22,8 @@ namespace Business.Concrete
         }
         public IResult Add(Car car)
         {
-            if (car.Description.Length < 5)
-            {
-                return new ErrorResult(Messages.Invalid);
-            }
+
+            ValidationTool.Validate(new CarValidator(),car);
             _carDal.Add(car);
             return new SuccesResult(Messages.EntityAdded);
 
